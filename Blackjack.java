be7952f1c17a294dc315be28529a711eaa684007
wvaldez14 <wvaldez14@punahou.edu>
2014-03-07 14:22:22 -1000

    Working Condition Blackjack
    This code works. It is fully commented as well. I think I want to fix
    the interface a bit to improve the user friendship. I also want to add
    the Ace 1 or 11 card.

--- Blackjack.java
+++ Blackjack.java
@@ -2,12 +2,21 @@ public class Blackjack{
   public static void main (String[] args){
     System.out.println("Welcome to the Blackjack Simulator");
     System.out.println("If you would like to quit please type it in");
-    System.out.println("Type in whether you want to hit, stand, or quit");
-    System.out.println("Here are your cards...");
+    System.out.println("Type in whether you want to hit, stand, or quit"); //instructions
+    System.out.println("Aces are worth 1");
+    System.out.println(" ");
     
-    Dealer game = new Dealer();
-    game.fill();
-    game.deal();
+    Dealer game = new Dealer(); //creates a new dealer
+    game.fill(); //fills the deck
+    game.deal(); //deals the first hand
 
   }
 }
+
+// fix deck size *fixed
+// stand immediately outofbounds error *fixed
+// for loop over looping in dealer hand *fixed
+// doubling the players outcome hand * fixed
+// 1st choice stand problem still? * fixed
+// problem 1 hit... then stand doubling * fixed
+// maybe add the 1 or 11 for aces
--- Card.java
+++ Card.java
@@ -1,18 +1,35 @@
-public class Card{
+public class Card{ //card class encharge of keeping the int value of the cards, the suit, and the interface for the face cards
   private int value;
   private String suit;
-  public Card(int v, String s){
+  public Card(int v, String s){ //Constructor from the arraylist of cards
     value = v;
     suit = s;
   }
-  public int getValue(){
+  public int getValue(){ // value of card
+    if(value>10){ //for the face cards which value is above 10, set the value as 10 (jacks,kings,queens)
+      value = 10;
+    }
     return value;
   }
-  public String getSuit(){
+  public String getSuit(){ //suit of card
     return suit;
   }
-  public String toString()
+  public String toString()// for the interface printing values and suit of cards
   {
+    if(value == 1){
+      return "The Ace of " + suit + "."; // Prints out Ace instead of 1
+    }
+    else if(value>10){
+      if(value == 11){
+        return "The Jack of " + suit + "."; //the face cards say its name instead of the numerical value the fill deck gave it
+      }
+      else if(value == 12){
+        return "The Queen of " + suit + ".";
+      }
+      else if(value == 13){
+        return "The King of " + suit + ".";
+      }
+    }
     return "The " + value + " of " + suit + ".";
   }
 }
